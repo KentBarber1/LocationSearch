d5edf095e1b5f6e7a17fcc55b698bd335e494eb7
Kent Barber <kbarber9@gatech.edu>
2022-10-13 22:14:45 -0400

    junits

--- backend/src/test/java/com/example/location/LocationApplicationTests.java
+++ backend/src/test/java/com/example/location/LocationApplicationTests.java
@@ -10,25 +10,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 class LocationApplicationTests {
 
 
-	// UserService userservice;
-	// User user;
+	UserService userservice = new UserService();
+	User user;
 
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void contextLoads() {
+	}
 
-	// public void createUserData() throws ExecutionException, InterruptedException {
-	// 	user.setUsername("kentster");
-	// 	user.setPassword("Password");
-	// 	userservice.createUser(user);
-	// }
+	public void createUserData() throws ExecutionException, InterruptedException {
+		User user = new User();
+		user.setUsername("kentster");
+		user.setPassword("Password");
+		userservice.createUser(user);
+	}
 
-	// @Test
-	// public void testUserData() throws ExecutionException, InterruptedException {
-	// 	createUserData();
-	// 	boolean res = userservice.existsUser("kentster");
-	// 	assertEquals(true, res);
-	
-	// }
+	@Test
+	public void testUserData() throws ExecutionException, InterruptedException {
+		createUserData();
+		boolean res = userservice.existsUser("kentster");
+		assertEquals(true, res);
+	}
+
+	@Test
+	public void testNotUser() throws ExecutionException, InterruptedException {
+		boolean res = userservice.existsUser("notHere");
+		assertEquals(false, res);
+	}
+
+	@Test
+	public void matchPasswords() throws ExecutionException, InterruptedException {
+		createUserData();
+		String res = userservice.getPasswordByUsername("kentster");
+		assertEquals("Password", res);
+	}
 }
